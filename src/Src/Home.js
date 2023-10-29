805b1914713337b1b9ab32c06e5b6ca8bdee4788
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-29 20:28:50 +0600

    ok

--- src/Src/Home.js
+++ src/Src/Home.js
@@ -5,11 +5,13 @@ import Footer from "../Components/Footer";
 import AboutUs from "../Components/AboutUs";
 import Services from "../Components/Services";
 import Testimonials from "../Components/Testimonials";
+import Slider from "../Components/Slider";
 
 export default function Home() {
   return (
     <div>
         <Navbar />
+        <Slider />
         <AboutUs />
         <Services />
         <Testimonials />
