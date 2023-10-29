b22ac3ad6ebb8abcafae543507920fb01b0b2d1f
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-29 20:55:07 +0600

    ok

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Testimonials() {
   return (
     <section id="Testimonials">
-    <h2>Testimonials</h2>
-    <p>Placeholder text Testimonials your company.</p>
-    {/* Add images and more content as needed */}
-  </section>
-  )
+      <h2>Testimonials</h2>
+      <p>Placeholder text Testimonials your company.</p>
+      <div>{/* Add images and more content as needed */}</div>
+    </section>
+  );
 }
